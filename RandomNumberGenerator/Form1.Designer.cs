3a2891bf0317e276f902054027634746e2a3db80
K <ca11iditas700@gmail.com>
2019-08-13 00:19:24 -0500

    Initial commit

--- RandomNumberGenerator/Form1.Designer.cs
+++ RandomNumberGenerator/Form1.Designer.cs
@@ -28,13 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonGenerateNumber = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 31);
+            this.label1.TabIndex = 0;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 31);
+            this.label2.TabIndex = 1;
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(150, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 31);
+            this.label3.TabIndex = 2;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(219, 13);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 31);
+            this.label4.TabIndex = 3;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(288, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 31);
+            this.label5.TabIndex = 4;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonGenerateNumber
+            // 
+            this.buttonGenerateNumber.Location = new System.Drawing.Point(81, 79);
+            this.buttonGenerateNumber.Name = "buttonGenerateNumber";
+            this.buttonGenerateNumber.Size = new System.Drawing.Size(86, 45);
+            this.buttonGenerateNumber.TabIndex = 5;
+            this.buttonGenerateNumber.Text = "Generate numbers";
+            this.buttonGenerateNumber.UseVisualStyleBackColor = true;
+            this.buttonGenerateNumber.Click += new System.EventHandler(this.buttonGenerateNumber_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(173, 79);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(86, 45);
+            this.buttonExit.TabIndex = 6;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(369, 147);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonGenerateNumber);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonGenerateNumber;
+        private System.Windows.Forms.Button buttonExit;
     }
 }
 
--- RandomNumberGenerator/Form1.cs
+++ RandomNumberGenerator/Form1.cs
@@ -16,5 +16,35 @@ namespace RandomNumberGenerator
         {
             InitializeComponent();
         }
+
+        // eventhandler
+        private void buttonGenerateNumber_Click(object sender, EventArgs e)
+        {
+            int size = 5;
+            int index;
+
+            int[] randomNumbers = new int[size];
+
+            Random myRandom = new Random();
+
+            // generate random numbers 0 to 99 and assigns it to randomNumbers[index]
+            for (index = 0; index < randomNumbers.Length; index++)
+            {
+                randomNumbers[index] = myRandom.Next(100);
+            }
+
+            // display array elements
+            label1.Text = randomNumbers[0].ToString();
+            label2.Text = randomNumbers[1].ToString();
+            label3.Text = randomNumbers[2].ToString();
+            label4.Text = randomNumbers[3].ToString();
+            label5.Text = randomNumbers[4].ToString();
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
